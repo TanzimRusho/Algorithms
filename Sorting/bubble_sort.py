ca789916cd2a09212007e8e4de66240e7c89f747
Tanzim Ahmed <32319601+TanzimRusho@users.noreply.github.com>
2021-09-21 18:14:50 +0600

    Add docstring

--- Sorting/bubble_sort.py
+++ Sorting/bubble_sort.py
@@ -1,4 +1,7 @@
 def bubble_sort(input_list):
+    """Takes a list of numbers and return its sorted version 
+    in ascending order using bubble sorting"""
+    
     length = len(input_list)
     
     for i in range(length):
